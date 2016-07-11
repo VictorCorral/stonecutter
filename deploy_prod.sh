76922534d0f4bc15a9e84a3c7dec3de2329a30d2
aewelch <amyelwelch@gmail.com>
2016-07-11 12:49:30 +0100

    AW | updates to deploy_prod.sh for email sending

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
 ssh $REMOTE_USER@$SERVER_IP <<EOF
-  echo $REMOTE_PASSWORD | sudo docker stop stonecutter || echo 'Failed to stop stonecutter container'
-  sudo docker rm stonecutter || echo 'Failed to remove stonecutter container'
-  sudo docker rmi dcent/stonecutter
-  sudo docker run -d -v /var/stonecutter/config:/var/config \
+  docker stop stonecutter || echo 'Failed to stop stonecutter container'
+  docker rm stonecutter || echo 'Failed to remove stonecutter container'
+  docker rmi dcent/stonecutter
+  docker run -d -v /var/stonecutter/config:/var/config \
              --env-file=/var/stonecutter/config/stonecutter.env \
              -v /data/stonecutter/static \
-             -v /var/stonecutter/email_service \
+             -v /var/stonecutter/email_service/providers/mailgun \
+             -v /var/stonecutter/email_service/send_mail.sh:/var/stonecutter/email_service/send_mail.sh \
              -p 5000:5000 \
              --restart=on-failure \
              --link mongo:mongo --name stonecutter dcent/stonecutter

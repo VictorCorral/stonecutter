5b8d82ce60b6c524ce4ffca00dfd9f647b7d1e6a
aewelch <amyelwelch@gmail.com>
2016-07-11 14:24:17 +0100

    AW | change docker run command so email scripts are mounted as files, not directories

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-ssh $REMOTE_USER@$SERVER_IP <<EOF
+ssh -i ~/.ssh/stonecutter_dob_private_key $REMOTE_USER@$SERVER_IP <<EOF
   docker stop stonecutter || echo 'Failed to stop stonecutter container'
   docker rm stonecutter || echo 'Failed to remove stonecutter container'
   docker rmi dcent/stonecutter
   docker run -d -v /var/stonecutter/config:/var/config \
              --env-file=/var/stonecutter/config/stonecutter.env \
              -v /data/stonecutter/static \
-             -v /var/stonecutter/email_service/providers/mailgun \
+             -v /var/stonecutter/email_service/providers/mailgun:/var/stonecutter/email_service/providers/mailgun \
              -v /var/stonecutter/email_service/send_mail.sh:/var/stonecutter/email_service/send_mail.sh \
              -p 5000:5000 \
              --restart=on-failure \
